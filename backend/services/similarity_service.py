465494fb135c9fb777ec5982bb7e8e6a6d2be2cd
Said Herminio Huallanca Gomez <saidhuallancagomez@gmail.com>
2026-07-15 21:14:24 -0500

    similarity service updated

--- backend/services/similarity_service.py
+++ backend/services/similarity_service.py
@@ -15,28 +15,90 @@ class SimilarityService:
                 "Dataset is empty"
             )
 
+        # =====================================================
+        # NIVEL 1
+        # Coincidencia más específica
+        # =====================================================
+
         filtered = df[
 
-            (df["job_role"] ==
-             request.job_role)
+            (df["job_role"] == request.job_role)
 
             &
 
-            (df["experience_level"] ==
-             request.experience_level)
+            (df["experience_level"] == request.experience_level)
 
             &
 
-            (df["education_required"] ==
-             request.education_required)
+            (df["education_required"] == request.education_required)
+
+            &
+
+            (df["industry"] == request.industry)
+
+            &
+
+            (df["company_size"] == request.company_size)
 
         ]
+
+        # =====================================================
+        # NIVEL 2
+        # Relajamos company_size
+        # =====================================================
 
         if len(filtered) < 30:
 
             filtered = df[
-                df["job_role"]
-                == request.job_role
+
+                (df["job_role"] == request.job_role)
+
+                &
+
+                (df["experience_level"] == request.experience_level)
+
+                &
+
+                (df["education_required"] == request.education_required)
+
+                &
+
+                (df["industry"] == request.industry)
+
+            ]
+
+        # =====================================================
+        # NIVEL 3
+        # Relajamos industry
+        # =====================================================
+
+        if len(filtered) < 30:
+
+            filtered = df[
+
+                (df["job_role"] == request.job_role)
+
+                &
+
+                (df["experience_level"] == request.experience_level)
+
+                &
+
+                (df["education_required"] == request.education_required)
+
+            ]
+
+        # =====================================================
+        # NIVEL 4
+        # Sólo cargo
+        # =====================================================
+
+        if len(filtered) < 30:
+
+            filtered = df[
+
+                df["job_role"] == request.job_role
+
             ]
 
         if filtered.empty:
