0004b7f9c2607153940bf520b005d1a41a8c6d3b
Said Herminio Huallanca Gomez <saidhuallancagomez@gmail.com>
2026-07-15 19:51:33 -0500

    fix uuid

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -87,6 +87,28 @@ function openBlockedModal(data) {
 
 }
 
+function generateUUID() {
+
+    if (window.crypto && crypto.randomUUID) {
+
+        return crypto.randomUUID();
+
+    }
+
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+
+        const r = Math.random() * 16 | 0;
+
+        const v = c === "x"
+            ? r
+            : (r & 0x3 | 0x8);
+
+        return v.toString(16);
+
+    });
+
+}
+
 function getUserUUID() {
 
     const KEY = "ai_predict_uuid";
@@ -106,7 +128,7 @@ function getUserUUID() {
 
     }
 
-    const uuid = crypto.randomUUID();
+    const uuid = generateUUID();
 
     localStorage.setItem(KEY, uuid);
     localStorage.setItem(TIME_KEY, now.toString());
